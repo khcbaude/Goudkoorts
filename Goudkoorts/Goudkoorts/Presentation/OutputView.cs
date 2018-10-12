f754b3034baf18e847eabf44bdfffbe1af6b4855
Billyyy013 <bhfnooij@avans.nl>
2018-10-12 12:10:21 +0200

    hfd

--- Goudkoorts/Goudkoorts/Presentation/OutputView.cs
+++ Goudkoorts/Goudkoorts/Presentation/OutputView.cs
@@ -14,6 +14,14 @@ namespace Goudkoorts
             Console.WriteLine("|          Welcome to              |");
             Console.WriteLine("|           Goldfever              |");
             Console.WriteLine("|   Symbol      |     Meaning      |");
+            Console.WriteLine("|     -         |      Field       |");
+            Console.WriteLine("|     O         |     EmptyCart    |");
+            Console.WriteLine("|     0         |     FullCart     |");
+            Console.WriteLine("|     █         |     Warehouse    |");
+            Console.WriteLine("|     ░         |      Water       |");
+            Console.WriteLine("|  ╚ ╔ ╗ ╝      |     Switches     |");
+            Console.WriteLine("|   Numbers     |      Boat        |");
+            Console.WriteLine("|     ~         |   ReangentField  |");
             Console.WriteLine(" __________________________________ ");
         }
     }
